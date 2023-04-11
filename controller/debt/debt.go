e31178fca4fe097d228f6a6b29372d5396af4047
Nathan Ferreira <nathan.fsilva@pagar.me>
2023-04-10 21:31:36 -0300

    debit/ticket controller part2/2

--- controller/debt/debt.go
+++ controller/debt/debt.go
@@ -1,8 +1,13 @@
 package debt
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/ntferr/icash/entities"
+	http_err "github.com/ntferr/icash/http/error"
+	"github.com/ntferr/icash/pkg/snowflake"
 	"github.com/ntferr/icash/service/crud"
 )
 
@@ -24,12 +29,95 @@ func NewController(service crud.Contract[entities.Debt]) Controller {
 	}
 }
 
-func (d debt) FindAll(ctx *fiber.Ctx) error
+func (d debt) FindAll(ctx *fiber.Ctx) error {
+	debts, err := d.service.FindAll(entities.Debt{})
+	if err != nil {
+		log.Printf("failed to get all debts: %e", err)
+		return http_err.NotFound(err)
+	}
 
-func (d debt) FindByID(ctx *fiber.Ctx) error
+	return ctx.JSON(&debts)
 
-func (d debt) New(ctx *fiber.Ctx) error
+}
 
-func (d debt) Alter(ctx *fiber.Ctx) error
+func (d debt) FindByID(ctx *fiber.Ctx) error {
+	debtID := ctx.Params("id")
+	if err := snowflake.Validate(debtID); err != nil {
+		log.Printf("failed to validate debt id: %e", err)
+		return http_err.BadRequest(err)
+	}
 
-func (d debt) Remove(ctx *fiber.Ctx) error
+	debt, err := d.service.FindByID(entities.Debt{ID: debtID})
+	if err != nil {
+		log.Printf("failed to get debt by id %s: %e", debtID, err)
+		return http_err.NotFound(err)
+	}
+
+	return ctx.JSON(&debt)
+}
+
+func (d debt) New(ctx *fiber.Ctx) error {
+	var debt entities.Debt
+	if err := ctx.App().Config().JSONDecoder(ctx.Body(), debt); err != nil {
+		log.Printf("failed to decode debt: %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	id, err := snowflake.GenerateNew()
+	if err != nil {
+		log.Printf("failed to generate id %f", err)
+		return http_err.InternalServerError(err)
+	}
+
+	debt.ID = *id
+	if err = d.service.Insert(debt); err != nil {
+		log.Printf("failed to insert debt: %e", err)
+		return http_err.InternalServerError(err)
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": fmt.Sprintf("card %s succefuly added", debt.ID),
+	})
+}
+
+func (d debt) Alter(ctx *fiber.Ctx) error {
+	var debt entities.Debt
+	debtID := ctx.Params("id")
+	if err := snowflake.Validate(debtID); err != nil {
+		log.Printf("failed to validate debt id: %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	if err := ctx.App().Config().JSONDecoder(ctx.Body(), debt); err != nil {
+		log.Printf("failed to decode debt: %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	debt.ID = debtID
+
+	if err := d.service.Update(debt); err != nil {
+		log.Printf("failed to update debt %s", debtID)
+		return http_err.InternalServerError(err)
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": "debt succefuly updated",
+	})
+}
+
+func (d debt) Remove(ctx *fiber.Ctx) error {
+	debtID := ctx.Params("id")
+	if err := snowflake.Validate(debtID); err != nil {
+		log.Printf("failed to validate debt id %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	if err := d.service.Delete(entities.Debt{ID: debtID}); err != nil {
+		log.Printf("failed to delete debt %s", debtID)
+		return http_err.InternalServerError(err)
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": fmt.Sprintf("debt %s succefuly deleted", debtID),
+	})
+}
--- controller/ticket/ticket.go
+++ controller/ticket/ticket.go
@@ -1,8 +1,13 @@
 package ticket
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/ntferr/icash/entities"
+	http_err "github.com/ntferr/icash/http/error"
+	"github.com/ntferr/icash/pkg/snowflake"
 	"github.com/ntferr/icash/service/crud"
 )
 
@@ -24,12 +29,94 @@ func NewController(service crud.Contract[entities.Ticket]) Controller {
 	}
 }
 
-func (t ticket) FindAll(ctx *fiber.Ctx) error
+func (t ticket) FindAll(ctx *fiber.Ctx) error {
+	tickets, err := t.service.FindAll(entities.Ticket{})
+	if err != nil {
+		log.Printf("failed to find all tickets %e", err)
+		return http_err.NotFound(err)
+	}
 
-func (t ticket) FindByID(ctx *fiber.Ctx) error
+	return ctx.JSON(&tickets)
+}
 
-func (t ticket) New(ctx *fiber.Ctx) error
+func (t ticket) FindByID(ctx *fiber.Ctx) error {
+	ticketID := ctx.Params("id")
+	if err := snowflake.Validate(ticketID); err != nil {
+		log.Printf("failed to validate ticketID")
+		return http_err.BadRequest(err)
+	}
 
-func (t ticket) Alter(ctx *fiber.Ctx) error
+	ticket, err := t.service.FindByID(entities.Ticket{ID: ticketID})
+	if err != nil {
+		log.Printf("failed to find ticket by id %s : %e", ticketID, err)
+		return http_err.NotFound(err)
+	}
 
-func (t ticket) Remove(ctx *fiber.Ctx) error
+	return ctx.JSON(&ticket)
+}
+
+func (t ticket) New(ctx *fiber.Ctx) error {
+	var ticket entities.Ticket
+	if err := ctx.App().Config().JSONDecoder(ctx.Body(), ticket); err != nil {
+		log.Printf("failed to decode ticket: %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	ticketID, err := snowflake.GenerateNew()
+	if err != nil {
+		log.Printf("failed to generate id %f", err)
+		return http_err.InternalServerError(err)
+	}
+
+	ticket.ID = *ticketID
+	if err := t.service.Insert(ticket); err != nil {
+		log.Printf("failed to insert ticket %s: %e", *ticketID, err)
+		return http_err.InternalServerError(err)
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": fmt.Sprintf("ticket %s succefuly added", *ticketID),
+	})
+}
+
+func (t ticket) Alter(ctx *fiber.Ctx) error {
+	var ticket entities.Ticket
+	ticketID := ctx.Params("id")
+	if err := snowflake.Validate(ticketID); err != nil {
+		log.Printf("failed to validate ticket %s", ticketID)
+		return http_err.BadRequest(err)
+	}
+
+	if err := ctx.App().Config().JSONDecoder(ctx.Body(), ticket); err != nil {
+		log.Printf("failed to decode ticket %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	ticket.ID = ticketID
+	if err := t.service.Update(ticket); err != nil {
+		log.Printf("failed to update tikcet %s", ticketID)
+		return http_err.InternalServerError(err)
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": fmt.Sprintf("ticket %s succefuly updated", ticketID),
+	})
+
+}
+
+func (t ticket) Remove(ctx *fiber.Ctx) error {
+	ticketID := ctx.Params("id")
+	if err := snowflake.Validate(ticketID); err != nil {
+		log.Printf("failed to validate id: %e", err)
+		return http_err.BadRequest(err)
+	}
+
+	if err := t.service.Delete(entities.Ticket{ID: ticketID}); err != nil {
+		log.Printf("failed to delete ticket %s", ticketID)
+		return http_err.InternalServerError(err)
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": fmt.Sprintf("ticket %s succefuly deleted", ticketID),
+	})
+}
