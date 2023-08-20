a7db242e10213c1209dc12a37e4c55b4b46d1c30
Nathan Ferreira <nathan.fsilva@pagar.me>
2023-08-20 17:40:36 -0300

    add some err

--- http/error/handler.go
+++ http/error/handler.go
@@ -9,13 +9,19 @@ import (
 )
 
 const (
+	createSnowflakeResponse = "An error ocurred to create an snowflake id"
 	notFindAllBanksResponse = "An error ocurred to get all banks"
+	insertBankResponse      = "An error ocurred to insert bank"
 	defaultResponse         = "An error ocurred into the API"
 )
 
 func WriteResponseError(err error) error {
 	if errors.Is(err, project_errors.ErrToFind) {
-		return fiber.NewError(http.StatusInternalServerError, notFindAllBanksResponse)
+		return fiber.NewError(http.StatusInternalServerError, notFindAllBanksResponse, err.Error())
+	} else if errors.Is(err, project_errors.ErrToCreateSnowflake) {
+		return fiber.NewError(http.StatusInternalServerError, createSnowflakeResponse, err.Error())
+	} else if errors.Is(err, project_errors.ErrInsertBank) {
+		return fiber.NewError(http.StatusInternalServerError, insertBankResponse, err.Error())
 	} else {
 		return fiber.NewError(http.StatusInternalServerError, defaultResponse)
 	}
