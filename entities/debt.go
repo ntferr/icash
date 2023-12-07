b78107011bb35333a4d6ced37642d47e3eefa6c3
Nathan Ferreira <nathan.fsilva@pagar.me>
2023-12-07 19:03:51 -0300

    adjust docker, debt and installment

--- entities/debt.go
+++ entities/debt.go
@@ -1,17 +1,12 @@
 package entities
 
-import "time"
-
 type Debt struct {
 	ID           string        `json:"id" gorm:"primaryKey"`
 	CardID       string        `json:"card_id"`
 	TicketID     string        `json:"ticket_id"`
 	Name         string        `json:"name"`
 	Description  string        `json:"description"`
-	Value        float32       `json:"value"`
 	Recurrent    bool          `json:"recurrent"`
-	Paid         bool          `json:"paid"`
-	DueDate      time.Time     `json:"due_date"`
 	Ticket       Ticket        `json:"ticket" gorm:"foreignKey:TicketID"`
 	Installments []Installment `json:"installments" gorm:"foreignKey:CardID"`
 }
--- entities/installment.go
+++ entities/installment.go
@@ -1,7 +1,8 @@
 package entities
 
 type Installment struct {
-	ID     string `json:"id"`
-	CardID string `json:"card_id"`
-	Number int    `json:"number"`
+	ID      string `json:"id" gorm:"primaryKey"`
+	DueDate string `json:"due_date" gorm:"dueDate"`
+	Paid    bool   `json:"paid" gorm:"paid"`
+	Number  int    `json:"number" gorm:"number"`
 }
