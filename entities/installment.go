844acc32134c06efa23ac1cf0e816a953a915bb9
Nathan Ferreira <nathan.fsilva@pagar.me>
2023-12-07 19:07:48 -0300

    adjust installment and ticket entities

--- entities/installment.go
+++ entities/installment.go
@@ -2,6 +2,7 @@ package entities
 
 type Installment struct {
 	ID      string `json:"id" gorm:"primaryKey"`
+	DebtID  string `json:"debt_id"`
 	DueDate string `json:"due_date" gorm:"dueDate"`
 	Paid    bool   `json:"paid" gorm:"paid"`
 	Number  int    `json:"number" gorm:"number"`
--- entities/ticket.go
+++ entities/ticket.go
@@ -2,5 +2,6 @@ package entities
 
 type Ticket struct {
 	ID       string `json:"id" gorm:"primaryKey"`
+	DebtID   string `json:"debt_id"`
 	CodeBars string `json:"code_bars"`
 }
