3d38d3078196db6a40ad70186bbbfe74033cc114
ntferr <ntfm95@gmail.com>
2024-09-01 21:44:21 -0300

    refactor router

--- http/router.go
+++ http/router.go
@@ -3,49 +3,29 @@ package http
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/ntferr/icash/controller"
-	"github.com/ntferr/icash/controller/bank"
-	"github.com/ntferr/icash/controller/card"
-	"github.com/ntferr/icash/controller/debt"
-	"github.com/ntferr/icash/controller/ticket"
 )
+
+type ControllerInterface interface {
+	FindAll(ctx *fiber.Ctx) error
+	FindByID(ctx *fiber.Ctx) error
+	New(ctx *fiber.Ctx) error
+	Alter(ctx *fiber.Ctx) error
+	Remove(ctx *fiber.Ctx) error
+}
 
 func SetupRouter(api fiber.Router, c controller.Controllers) {
 	api.Get("/health-check", c.Health.Status)
 
-	bankRouter(api.Group("/banks"), c.Bank)
-	cardRouter(api.Group("/cards"), c.Card)
-	ticketRouter(api.Group("/tickets"), c.Ticket)
-	debtRouter(api.Group("/debts"), c.Debt)
+	resgisterRoutes(api.Group("/banks"), c.Bank)
+	resgisterRoutes(api.Group("/cards"), c.Card)
+	resgisterRoutes(api.Group("/tickets"), c.Ticket)
+	resgisterRoutes(api.Group("/debts"), c.Debt)
 }
 
-func bankRouter(router fiber.Router, b bank.Controller) {
-	router.Get("/", b.FindAll)
-	router.Get("/:id", b.FindByID)
-	router.Post("", b.New)
-	router.Put("/:id", b.Alter)
-	router.Delete("/:id", b.Remove)
-}
-
-func cardRouter(router fiber.Router, c card.Controller) {
-	router.Get("/", c.FindAll)
-	router.Get("/:id", c.FindByID)
-	router.Post("", c.New)
-	router.Put("/:id", c.Alter)
-	router.Delete("/:id", c.Remove)
-}
-
-func ticketRouter(router fiber.Router, t ticket.Controller) {
-	router.Get("/", t.FindAll)
-	router.Get("/:id", t.FindByID)
-	router.Post("", t.New)
-	router.Put("/:id", t.Alter)
-	router.Delete("/:id", t.Remove)
-}
-
-func debtRouter(router fiber.Router, d debt.Controller) {
-	router.Get("/", d.FindAll)
-	router.Get("/:id", d.FindByID)
-	router.Post("", d.New)
-	router.Put("/:id", d.Alter)
-	router.Delete("/:id", d.Remove)
+func resgisterRoutes(router fiber.Router, ctrl ControllerInterface) {
+	router.Get("/", ctrl.FindAll)
+	router.Get("/:id", ctrl.FindByID)
+	router.Post("", ctrl.New)
+	router.Put("/:id", ctrl.Alter)
+	router.Delete("/:id", ctrl.Remove)
 }
